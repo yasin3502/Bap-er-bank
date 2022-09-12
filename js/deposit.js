561bd2467d21cf6372d36ce21267312cf3ddcc99
yasin3502 <yasinsarder52@gmail.com>
2022-09-12 23:16:25 +0600

    deposit and total balance

--- js/deposit.js
+++ js/deposit.js
@@ -1,13 +1,24 @@
 document.getElementById('btn-deposit').addEventListener('click', function () {
+    console.log("object");
     const depositField = document.getElementById('deposit-field');
-    const newDepositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
     const depositTotalElement = document.getElementById('deposit-total');
-    const previousDepositTotal = depositTotalElement.innerText;
+
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
 
     const currentDepositTotal = previousDepositTotal + newDepositAmount;
 
     depositTotalElement.innerText = currentDepositTotal;
 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    balanceTotalElement.innerText = currentBalanceTotal;
     //clear the deposit field
     depositField.value = '';
 
